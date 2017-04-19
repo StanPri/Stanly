2877371bb5cccd276ad1a681c560336eba171277
StanPri <spribytkovsky@yahoo.com>
2017-04-19 13:09:56 -0700

    MovieController ActionResult added

--- Stanly/Controllers/MovieController.cs
+++ Stanly/Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Stanly.Models;
 
 namespace Stanly.Controllers
@@ -13,7 +14,22 @@ namespace Stanly.Controllers
         public ActionResult RandomMovie()
         {
             var movie = new Movie(){ Name = "Hulk" };
-            return View(movie);
+            return RedirectToAction("RandomBook", "Book");
+        }
+
+        public ActionResult SearchMovie(int? pageNumber, string sortBy)
+        {
+            if (!pageNumber.HasValue)
+            {
+                pageNumber = 1;
+
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageNumber={0}, SortBy={1}", pageNumber, sortBy));
         }
     }
 }
